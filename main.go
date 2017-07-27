0e4f0a5e678512a89a4d9011e8f896d807c992c8
Ben Marshall <ben@benmarshall.co.uk>
2017-07-28 10:18:02 +1200

    Efficient drawing of sprites

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"math"
 	"math/rand"
 	"time"
@@ -19,7 +20,7 @@ func run() {
 	cfg := pixelgl.WindowConfig{
 		Title:  "Pixel Rocks!",
 		Bounds: pixel.R(0, 0, 1024, 768),
-		VSync:  true,
+		VSync:  false,
 	}
 
 	// Create the window
@@ -33,6 +34,7 @@ func run() {
 	if err != nil {
 		panic(err)
 	}
+	batch := pixel.NewBatch(&pixel.TrianglesData{}, spritesheet)
 	var treeFrames []pixel.Rect
 	for x := spritesheet.Bounds().Min.X; x < spritesheet.Bounds().Max.X; x += 32 {
 		for y := spritesheet.Bounds().Min.Y; y < spritesheet.Bounds().Max.Y; y += 32 {
@@ -45,14 +47,21 @@ func run() {
 		camSpeed     = 500.0
 		camZoom      = 1.0
 		camZoomSpeed = 1.2
-		trees        []*pixel.Sprite
-		matrices     []pixel.Matrix
+		frames       = 0
+		second       = time.Tick(time.Second)
 	)
 
 	lastTime := time.Now()
 
 	// Keep going till the window is closed
 	for !win.Closed() {
+		frames++
+		select {
+		case <-second:
+			win.SetTitle(fmt.Sprintf("%s | FPS: %d", cfg.Title, frames))
+			frames = 0
+		default:
+		}
 		// Update the logic
 		dt := time.Since(lastTime).Seconds()
 		lastTime = time.Now()
@@ -60,11 +69,10 @@ func run() {
 		win.SetMatrix(cam)
 
 		// Capture user input
-		if win.JustPressed(pixelgl.MouseButtonLeft) {
+		if win.Pressed(pixelgl.MouseButtonLeft) {
 			tree := pixel.NewSprite(spritesheet, treeFrames[rand.Intn(len(treeFrames))])
-			trees = append(trees, tree)
 			mouse := cam.Unproject(win.MousePosition())
-			matrices = append(matrices, pixel.IM.Scaled(pixel.ZV, 4).Moved(mouse))
+			tree.Draw(batch, pixel.IM.Scaled(pixel.ZV, 4).Moved(mouse))
 		}
 		if win.Pressed(pixelgl.KeyLeft) {
 			camPos.X -= camSpeed * dt
@@ -84,9 +92,7 @@ func run() {
 		win.Clear(colornames.Forestgreen)
 
 		// Draw a trees
-		for i, tree := range trees {
-			tree.Draw(win, matrices[i])
-		}
+		batch.Draw(win)
 
 		// Update the window
 		win.Update()
