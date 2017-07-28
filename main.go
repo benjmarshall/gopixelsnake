3031d4753051e1aa8f14d3651f808366c95e9a9b
Ben Marshall <ben@benmarshall.co.uk>
2017-07-28 13:02:32 +1200

    Basic drawing done

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"github.com/faiface/pixel"
+	"github.com/faiface/pixel/imdraw"
 	"github.com/faiface/pixel/pixelgl"
 	"golang.org/x/image/colornames"
 )
@@ -24,11 +25,32 @@ func run() {
 		panic(err)
 	}
 
+	// Setup Game Configuration
+	gameCFG := newGameCFG(gameAreaDimsType{700, 700}, 5, cfg)
+
+	// Create Game Area Shape
+	imd := imdraw.New(nil)
+	imd.Color = colornames.White
+	imd.Push(gameCFG.gameArea.Min, gameCFG.gameArea.Max)
+	imd.Rectangle(2)
+
+	// Initialize a new snake
+	snake := newSnake(gameCFG)
+
+	// Create a snake shape
+	imd.Color = colornames.Purple
+	imd.Push(snake.getHeadPos(), snake.getTailPos())
+	imd.Line(gameCFG.gameGridSize)
+
 	// Keep going till the window is closed
 	for !win.Closed() {
 
 		// Clear the frame
 		win.Clear(colornames.Darkcyan)
+
+		// Draw the Game Area
+		imd.Draw(win)
+
 		// Update the window
 		win.Update()
 	}
